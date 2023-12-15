da14af5c6b7703210806880e240000a147363b70
Matt <matthieu.laine@ecole2600.com>
2023-12-15 15:16:28 +0100

    modifications script lancement vmé

--- script_matt.sh
+++ script_matt.sh
@@ -9,9 +9,9 @@ fi
 
 sudo losetup -Pf /home/matt/Documents/ecole2600/2S1/ProjetRootKit/kernel_rootkit/disk_project/disk.img
 mkdir -p /tmp/my-rootfs
-sudo mount /dev/loop3p1 /tmp/my-rootfs
+sudo mount /dev/loop5p1 /tmp/my-rootfs
 sudo cp rootest.ko /tmp/my-rootfs/
 sudo umount /tmp/my-rootfs
-sudo losetup -d /dev/loop3
+sudo losetup -d /dev/loop5
 
 qemu-system-x86_64 -hda /home/matt/Documents/ecole2600/2S1/ProjetRootKit/kernel_rootkit/disk_project/disk.img -nographic
